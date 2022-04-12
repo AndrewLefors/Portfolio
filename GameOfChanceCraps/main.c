45c997feb8964c235b477b4ed1a366bac92f3904
AndrewLefors <35347055+AndrewLefors@users.noreply.github.com>
2022-04-11 20:26:53 -0700

    Uploaded main file for GOCraps

---
+++ GameOfChanceCraps/main.c
@@ -0,0 +1,33 @@
+/*
+
+Created By: Andrew Lefors
+Created On: 2/24/2020
+Project: Game of Chance - Craps
+File Version: 0.2.30.20
+
+Description: This is the game of Craps, where the user will be prompted for a bank balance from which wagers can be made.
+				The user will place bets and roll the die, being mocked along the way by an ugly toad. No files are saved.
+
+
+
+
+*/
+
+#include "GOCraps.h"
+
+
+int main (void)
+{
+	//Set Seed for rand
+	//srand((unsigned int)time(NULL));
+	//Start text-based game
+	//start_game();
+	//Start animated version. DID NOT FINISH
+	start_sdl();
+
+	
+
+	
+
+	return 0;
+}
