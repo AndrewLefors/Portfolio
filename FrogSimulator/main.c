fb3860f91637bcd8930035a89d8fa65b6b4e5763
AndrewLefors <35347055+AndrewLefors@users.noreply.github.com>
2022-04-11 20:17:04 -0700

    Uploaded main file for FrogSimulator in C

---
+++ FrogSimulator/main.c
@@ -0,0 +1,29 @@
+/*
+
+Created By: Andrew Lefors
+Created On: 2/24/2020
+Project: Frog Simulator
+File Version: 0.2.30.20
+
+Description: This is the Frog life Simulator, written in C using the library SDL_C. Boundaries are set, animations respond to movement direction appropriatly.
+
+
+			NOTE: Frog Sprites acquired from rpgtileset.com
+			NOTE: Animation background acquired from img-itchzone.com
+			NOTE: Watched tutorials on SDL2 from numerous locations for animations.
+
+
+*/
+
+#include "GOCraps.h"
+
+
+int main (void)
+{
+	//Start animated version.
+	start_sdl();
+
+	
+
+	return 0;
+}
