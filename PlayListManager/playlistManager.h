703290a51116ada69545f1a2f714df966f71142c
AndrewLefors <35347055+AndrewLefors@users.noreply.github.com>
2022-04-11 20:42:34 -0700

    Updated header comments for clarity

--- PlayListManager/playlistManager.h
+++ PlayListManager/playlistManager.h
@@ -1,19 +1,18 @@
 /***************************************************************************************************************************
 *                                                                                                                          *
 * Created By: Andrew Lefors                                                                                                *
-* Created On: 5/15/2020                                                                                                    *
-* Assignment: PA-1 'Analyze FitBit Data'                                                                                   *
-* Created For: Dr. Guizani CPTS_122 Summer 2020                                                                            *
-* File Version: 0.5.15.20                                                                                                  *
-* File Description: This program opens a csv-file named "FitbitData.csv", reads in data line-by-line, separates the fields *                                                                                                       *
-* 	                checks for duplicates, and cleanses missing valus. Then the data is passed into a struct, analyzed,    *
-*                   and results are output to the console and a file "Results.csv"                                         *
+* Created On: 5/20/2020                                                                                                    *
+* Project: Playlist Manager                                                                                                *
+* File Version: 0.5.20.20                                                                                                  *
+* File Description: This program opens a csv-file named "moviePlayList.csv", reads in data line-by-line, separates fields, *                                                                                                       *
+* 	            checks for duplicates, and organizes the results. The list can be updated in the program without taking*
+*                   effect until the program is finished to reduce write time. List can be dynamically updated.            *
 *                                                                                                                          *
-*       ~  ~                                                                                                               *
-*       @..@                                                                                                               *
-*	   (-----)                                                                                                             *
-*	  { )_J_( }                                                                                                            *
-*	<^>|>   <|<^>                                                                                                          *
+*           ~  ~                                                                                                           *
+*           @..@                                                                                                           *
+*	   (-----)                                                                                                         *
+*	  { )_J_( }                                                                                                        *
+*	<^>|>   <|<^>                                                                                                      *
 ****************************************************************************************************************************/
 
 #ifndef PLAYLIST_MANAGER
