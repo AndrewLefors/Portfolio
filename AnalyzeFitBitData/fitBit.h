f496aa4b86fc97c0dbcb4f0693dbe0d54f538d12
AndrewLefors <35347055+AndrewLefors@users.noreply.github.com>
2022-04-11 20:34:37 -0700

    Updated Header Comments

--- AnalyzeFitBitData/fitBit.h
+++ AnalyzeFitBitData/fitBit.h
@@ -2,18 +2,17 @@
 *                                                                                                                          *
 * Created By: Andrew Lefors                                                                                                *
 * Created On: 5/15/2020                                                                                                    *
-* Assignment: PA-1 'Analyze FitBit Data'                                                                                   *
-* Created For: Dr. Guizani CPTS_122 Summer 2020                                                                            *
+* Project: FitBit Data Analysis                                                                                            *
 * File Version: 0.5.15.20                                                                                                  *
 * File Description: This program opens a csv-file named "FitbitData.csv", reads in data line-by-line, separates the fields *                                                                                                       *
-* 	                checks for duplicates, and cleanses missing valus. Then the data is passed into a struct, analyzed,    *
-*                   and results are output to the console and a file "Results.csv"                                         *
+* 	                checks for duplicates, and cleanses missing valus. Then the data is passed into a struct, analyzed,*
+*                   and results are output to the console to a file "Results.csv"                                          *
 *                                                                                                                          *
-*       ~  ~                                                                                                               *
-*       @..@                                                                                                               *
-*	   (-----)                                                                                                             *
-*	  { )_J_( }                                                                                                            *
-*	<^>|>   <|<^>                                                                                                          *
+*           ~  ~                                                                                                           *
+*           @..@                                                                                                           *
+*	   (-----)                                                                                                         *
+*	  { )_J_( }                                                                                                        *
+*	<^>|>   <|<^>                                                                                                      *
 ****************************************************************************************************************************/
 #define _CRT_SECURE_NO_WARNINGS // necessary to ignore scanf_s () warnings (Must come first)
 #include <stdio.h>
